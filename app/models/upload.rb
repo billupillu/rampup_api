4822449b569c01865f90f2c190f6435d2aaebc71
Sumeet Masih <sumeet.masih@outlook.com>
2019-03-11 12:50:31 +0530

    added the demarcation of iframe

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,7 +4,7 @@ class Upload < ApplicationRecord
   validates_attachment :file, presence: true, content_type: { content_type: "application/pdf" }#, if: Proc.new {|a| a.info.present? }  presence: true,
 
   def g_iframe(dm)
-  	i_frame = "<div class='row'><iframe src='#{dm}#{self.file.url}' data-uploadid='#{self.id}'></iframe></div>"
+  	i_frame = "<div class='row'><iframe src='#{dm}#{self.file.url}' data-uploadid='#{self.id}' style='width:100%;height:100%;'></iframe></div>"
   	desc = ''
   	reader = PDF::Reader.new(self.file.path)
   	reader.pages.each do |page| 
